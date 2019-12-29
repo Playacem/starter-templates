1d47a0316b75ee228071808a527e7c04d3a66cd0
Playacem <playacem@gmail.com>
2019-12-29 14:59:36 +0100

    Cleanup files

--- kotlin-application-with-gradle/build.gradle.kts
+++ kotlin-application-with-gradle/build.gradle.kts
@@ -1,35 +1,35 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.61"
-    application
+	id("org.jetbrains.kotlin.jvm") version "1.3.61"
+	application
 	id("com.github.johnrengelman.shadow") version "5.2.0"
 	id("de.fayard.buildSrcVersions") version "0.6.1"
 }
 
 repositories {
-    mavenCentral()
-    jcenter()
+	mavenCentral()
+	jcenter()
 }
 
 dependencies {
-    // Align versions of all Kotlin components
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+	// Align versions of all Kotlin components
+	implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
 
-    // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	// Use the Kotlin JDK 8 standard library.
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // Use the Kotlin test library.
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+	// Use the Kotlin test library.
+	testImplementation("org.jetbrains.kotlin:kotlin-test")
 
-    // Use the Kotlin JUnit integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+	// Use the Kotlin JUnit integration.
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
 application {
-    // Define the main class for the application
+	// Define the main class for the application
 	group = "TODO"
 	version = App.VERSION
 	applicationName = "Template-TODO"
-    mainClassName = "TODO"
+	mainClassName = "TODO"
 }
 
 tasks {
@@ -42,4 +42,3 @@ tasks {
 		distributionType = Wrapper.DistributionType.ALL
 	}
 }
-
